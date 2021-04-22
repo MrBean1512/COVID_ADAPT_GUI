10b430a119c50e68cced71fe12c0a34278b67887
Ben Michael <michaben@live.com>
2021-04-22 13:30:57 -0700

    add button loop and input validation to settings

--- src/Window_Settings.py
+++ src/Window_Settings.py
@@ -4,6 +4,7 @@ import tkinter.font
 from tkinter.constants import *
 from tkinter.filedialog import askopenfile
 import json
+import Button_Builder
 
 # subclass of Entry that holds the name of the variable in the settings file that the entry corresponds with
 class settingEntry(tkinter.Entry):
@@ -11,6 +12,7 @@ class settingEntry(tkinter.Entry):
         tkinter.Entry.__init__(self, master, *args, **kwargs)
         self.variableName = "unnamed"
         self.default = ""
+        self.type = int
 
 def window_Settings():
     tk = tkinter.Tk()
@@ -38,6 +40,7 @@ def window_Settings():
     entrySteepnessExposure = settingEntry(rightFrame)
     entrySteepnessExposure.variableName = "steepness_exposure"
     entrySteepnessExposure.default = 1
+    entrySteepnessExposure.type = "decimal"
     entrySteepnessExposure.pack(side = TOP)
     
     #define steepness infectious entry
@@ -46,6 +49,7 @@ def window_Settings():
     entrySteepnessInfectious = settingEntry(rightFrame)
     entrySteepnessInfectious.variableName = "steepness_infectious"
     entrySteepnessInfectious.default = 1
+    entrySteepnessInfectious.type = "decimal"
     entrySteepnessInfectious.pack(side = TOP)
 
     # define midpoint infectious entry
@@ -54,6 +58,7 @@ def window_Settings():
     entryMidpointInfectious = settingEntry(rightFrame)
     entryMidpointInfectious.variableName = "midpoint_infectious"
     entryMidpointInfectious.default = 7200
+    entryMidpointInfectious.type = "decimal"
     entryMidpointInfectious.pack(side = TOP)
 
     # define steepness recovery
@@ -62,6 +67,7 @@ def window_Settings():
     entrySteepnessRecovery = settingEntry(rightFrame)
     entrySteepnessRecovery.variableName = "steepness_recovery"
     entrySteepnessRecovery.default = 1
+    entrySteepnessRecovery.type = "decimal"
     entrySteepnessRecovery.pack(side = TOP)
 
     # define midpoint recovery
@@ -70,6 +76,7 @@ def window_Settings():
     entryMidpointRecovery = settingEntry(rightFrame)
     entryMidpointRecovery.variableName = "midpoint_recovery"
     entryMidpointRecovery.default = 14400
+    entryMidpointRecovery.type = "decimal"
     entryMidpointRecovery.pack(side = TOP)
 
     # define virus decay rate
@@ -78,6 +85,7 @@ def window_Settings():
     entryVirusDecayRate = settingEntry(rightFrame)
     entryVirusDecayRate.variableName = "virus_decay_rate"
     entryVirusDecayRate.default = 0.001
+    entryVirusDecayRate.type = "decimal"
     entryVirusDecayRate.pack(side = TOP)
 
     # define number infectious
@@ -86,6 +94,7 @@ def window_Settings():
     entryNumberInfectious = settingEntry(rightFrame)
     entryNumberInfectious.variableName = "number_infectious"
     entryNumberInfectious.default = 4
+    entryNumberInfectious.type = "int"
     entryNumberInfectious.pack(side = TOP)
 
     # define number susceptible
@@ -94,6 +103,7 @@ def window_Settings():
     entryNumberSusceptible = settingEntry(rightFrame)
     entryNumberSusceptible.variableName = "number_susceptible"
     entryNumberSusceptible.default = 6
+    entryNumberSusceptible.type = "int"
     entryNumberSusceptible.pack(side = TOP)
 
     # define gridsize x
@@ -102,6 +112,7 @@ def window_Settings():
     entryGridsizeX = settingEntry(rightFrame)
     entryGridsizeX.variableName = "gridsize_x"
     entryGridsizeX.default = 43
+    entryGridsizeX.type = "int"
     entryGridsizeX.pack(side = TOP)
 
     # define gridsize y
@@ -110,6 +121,7 @@ def window_Settings():
     entryGridsizeY = settingEntry(rightFrame)
     entryGridsizeY.variableName = "gridsize_y"
     entryGridsizeY.default = 43
+    entryGridsizeY.type = "int"
     entryGridsizeY.pack(side = TOP)
 
     # define max time
@@ -118,6 +130,7 @@ def window_Settings():
     entryMaxTime = settingEntry(rightFrame)
     entryMaxTime.variableName = "max_time"
     entryMaxTime.default = 30000
+    entryMaxTime.type = "int"
     entryMaxTime.pack(side = TOP)
 
     # define midpoint exposure
@@ -126,29 +139,41 @@ def window_Settings():
     entryMidpointExposure = settingEntry(rightFrame)
     entryMidpointExposure.variableName = "midpoint_exposure"
     entryMidpointExposure.default = 50
+    entryMidpointExposure.type = "int"
     entryMidpointExposure.pack(side = TOP)
+
+    #define error message
+    labelErrorMessage = tkinter.Label(bottomFrame, text = "", fg = "#f00")
+    labelErrorMessage.pack(side = TOP)
 
     # store all entry objects in a list
     entries = [widget for widget in rightFrame.winfo_children() if isinstance(widget, settingEntry)]
-
-    # define back  button
-    # closes the settings window and returns to the main window
-    backButton = tkinter.Button(bottomFrame, text = "Return To Menu", command = tk.destroy)
-    backButton.pack(side = BOTTOM)
 
     # define save button
     # the save button creates a map containing every setting and value and outputs the map to a JSON file
     settings = {}
     def saveSettings():
+        badInput = FALSE
+        labelErrorMessage['text'] = ""
+
         for entry in entries:
-            settings[entry.variableName] = entry.get() 
+            settings[entry.variableName] = entry.get()
 
-        outFile = open("settings.json", "w")
-        outFile.write(json.dumps(settings, indent = 0))
-        outFile.close()
+            if entry.type == "decimal":
+                decimalRemove = settings[entry.variableName].replace(".", "", 1) # remove up to one decimal point. 
+                # a valid positive decimal number has up to one decimal and all other characters are digits
+                if not decimalRemove.isdigit():
+                        badInput = TRUE
+                        labelErrorMessage['text'] = str(entry.variableName) + " must be a positive decimal value"
+            else: #entry.type is int
+                if not settings[entry.variableName].isdigit():
+                        badInput = TRUE
+                        labelErrorMessage['text'] = str(entry.variableName) + " must be a positive integer value"
 
-    saveButton = tkinter.Button(bottomFrame, text = "Save Settings", command = saveSettings)
-    saveButton.pack(side = BOTTOM)
+        if not badInput:
+            outFile = open("settings.json", "w")
+            outFile.write(json.dumps(settings, indent = 0))
+            outFile.close()
 
     # define defaults button
     # default button sets all entries to default values and saves these new values to the settings file
@@ -169,8 +194,15 @@ def window_Settings():
             entry.delete(0, END)
             entry.insert(0, settingsIn[entry.variableName])
 
-    defaultButton = tkinter.Button(bottomFrame, text = "Set Default Values", command = setAsDefaults)
-    defaultButton.pack(side = BOTTOM)
+    buttonSpecs = [
+        #["name", function],
+        ["Return To Menu", tk.destroy],
+        ["Save Settings", saveSettings],
+        ["Set Default Values", setAsDefaults],
+    ]
+
+    Button_Builder.buttonBuilder(bottomFrame, buttonSpecs)
+
 
 
     #inialize display of settings
